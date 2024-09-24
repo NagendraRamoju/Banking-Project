5d8de5a76bdba7300d368c3a7e9f2b685989f007
NagendraRamoju <143198616+NagendraRamoju@users.noreply.github.com>
2024-09-24 15:49:27 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,24 @@
-resource "aws_instance" "test-servers" {
-  ami           = "ami-00beae93a2d981137" 
-  instance_type = "t2.nano" 
-  key_name = "newkeypairaws"
-  vpc_security_group_ids= ["sg-02c3613a7b03f8ce3"]
+resource "aws_instance" "test-server" {
+  ami           = "ami-0e86e20dae9224db8" 
+  instance_type = "t2.micro" 
+  key_name = "slavekey"
+  vpc_security_group_ids= ["sg-0135234a952876b5d"]
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("./newkeypairaws.pem")
+    private_key = file("./slavekey.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
     inline = [ "echo 'wait to start instance' "]
   }
   tags = {
-    Name = "test-servers"
+    Name = "test-server"
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-servers.public_ip} > inventory"
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
   }
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/CICD_JOB/terraform-files/ansibleplaybook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/project1/terraform-files/ansibleplaybook.yml"
         }
   }
